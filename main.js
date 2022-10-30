22e9213f72ab184a12c5e90ab445e84b2c8b98bf
Anas Alnagar <35266228+oniseg@users.noreply.github.com>
2022-10-30 17:22:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var img = document.querySelector(".img");
+var container = document.querySelector(".container")
+
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.background = color;
+}
